e999914319121703830f11c2e576c5d18aac5597
Ilaria <maffiolailaria1@gmail.com>
2025-04-14 16:41:28 +0200

    [ADD] add fullName and hobbies functions with example usage

---
+++ script.js
@@ -0,0 +1,6 @@
+const fullName = require('./moduli/names');
+
+console.log(fullName('ilaria', 'maffiola'));
+
+const hobbies = require('./moduli/hobbies');
+console.log(hobbies('leggere', 'fare sport', 'ballare'));
