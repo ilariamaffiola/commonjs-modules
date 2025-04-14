e4854c28304eb156e125c4ef499d83a256d57c8a
Ilaria <maffiolailaria1@gmail.com>
2025-04-14 16:34:12 +0200

    [UPD]add getFullName function and example usage

---
+++ modules/names.js
@@ -0,0 +1,8 @@
+function getFullName(firstName, lastName){
+    let objFullName = {
+        nome: firstName,
+        cognome: lastName,
+    };
+    return objFullName;
+};
+module.exports = getFullName; 
